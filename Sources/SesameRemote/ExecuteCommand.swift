fcf6a5db8e82a06260506d620db97ed6f9687620
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-05-18 17:33:19 +0900

    add ExecuteCommand

---
+++ Sources/SesameRemote/ExecuteCommand.swift
@@ -0,0 +1,71 @@
+import Foundation
+import HTTPTypes
+import HTTPTypesFoundation
+import Crypto
+
+extension Client {
+  public func execute(
+    command: Command,
+    deviceId: UUID,
+    deviceSecretKey: String,
+  ) async throws -> ExecuteCommandResult {
+    let url = baseURL
+      .appending(path: "/api/sesame2/\(deviceId)/cmd")
+    
+    let timestamp = UInt32(Date.now.timeIntervalSince1970)
+    let timestampSlice = timestamp.data[1..<4]
+    
+    let sign = try AES.GCM.seal(
+      timestampSlice,
+      using: .init(data: deviceSecretKey.data(using: .utf8)!)
+    )
+    
+    let command = CommandBody(
+      command: command,
+      history: "test".data(using: .utf8)!.base64EncodedString(),
+      sign: sign.tag.base64EncodedString()
+    )
+    
+    let body = try JSONEncoder().encode(command)
+    
+    let request = HTTPRequest(
+      method: .post,
+      url: url,
+      headerFields: [.xApiKey: apiKey]
+    )
+    
+    let (data, _) = try await httpClient.execute(for: request, from: body)
+    let result = try JSONDecoder().decode(ExecuteCommandResult.self, from: data)
+    return result
+  }
+}
+
+public struct ExecuteCommandResult: Codable {
+  public var statusCode: Int
+}
+
+
+private struct CommandBody: Encodable {
+  var command: Command
+  var history: String
+  var sign: String
+  
+  enum CodingKeys: String, CodingKey {
+    case command = "cmd"
+    case history
+    case sign
+  }
+}
+
+public enum Command: UInt, Codable {
+  case toggle = 88
+  case lock = 82
+  case unlock = 83
+}
+
+extension UInt32 {
+  fileprivate var data: Data {
+    var int = self
+    return Data(bytes: &int, count: MemoryLayout<Self>.size)
+  }
+}
