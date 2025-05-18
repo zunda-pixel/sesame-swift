8998bc04b50372cede643319c5a66edd66f1724c
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-05-18 17:33:08 +0900

    add test

--- Tests/SesameRemoteTests/SesameRemoteTests.swift
+++ Tests/SesameRemoteTests/SesameRemoteTests.swift
@@ -1,6 +1,33 @@
+import Foundation
 import Testing
-@testable import SesameRemote
+import SesameRemote
 
-@Test func example() async throws {
-  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+let client = Client(
+  httpClient: .urlSession(.shared),
+  apiKey: ProcessInfo.processInfo.environment["API_KEY"]!
+)
+
+let deviceId = UUID(uuidString: ProcessInfo.processInfo.environment["DEVICE_ID"]!)!
+let devviceSecretKey = ProcessInfo.processInfo.environment["DEVICE_SECRET_KEY"]!
+
+@Test
+func getDevice() async throws {
+  let device = try await client.device(deviceId)
+  print(device)
+}
+
+@Test
+func getHistories() async throws {
+  let histories = try await client.histories(deviceId: deviceId)
+  print(histories)
+}
+
+@Test
+func executeCommand() async throws {
+  let result = try await client.execute(
+    command: .lock,
+    deviceId: deviceId,
+    deviceSecretKey: devviceSecretKey
+  )
+  print(result)
 }
