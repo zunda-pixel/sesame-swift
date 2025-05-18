833f5045b689bd28b63e3b58ee7a55654ff42025
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-05-18 11:54:33 +0900

    split Local and Remote

--- Package.swift
+++ Package.swift
@@ -4,19 +4,42 @@ import PackageDescription
 
 let package = Package(
   name: "sesame-swift",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+  ],
   products: [
     .library(
       name: "Sesame",
-      targets: ["Sesame"]
+      targets: [
+        "SesameRemote",
+        "SesameLocal"
+      ]
     ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.1"),
+    .package(url: "https://github.com/zunda-pixel/http-client.git", from: "0.3.0"),
   ],
   targets: [
     .target(
-      name: "Sesame"
+      name: "SesameLocal"
+    ),
+    .target(
+      name: "SesameRemote",
+      dependencies: [
+        .product(name: "HTTPClient", package: "http-client"),
+        .product(name: "HTTPTypes", package: "swift-http-types"),
+        .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
+      ]
     ),
     .testTarget(
-      name: "SesameTests",
-      dependencies: ["Sesame"]
+      name: "SesameRemoteTests",
+      dependencies: ["SesameRemote"]
+    ),
+    .testTarget(
+      name: "SesameLocalTests",
+      dependencies: ["SesameLocal"]
     ),
   ]
 )
